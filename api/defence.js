76e4b7739240ba3d3a413bdfd6d53c7d456f82ad
Mojtaba <rabiei.mojtaba@gmail.com>
2025-05-24 22:03:00 +0330

    Update defence.js

--- api/defence.js
+++ api/defence.js
@@ -4,7 +4,7 @@ const meshy = require('../meshy');
 
 module.exports = function(store, broadcast) {
   const router = express.Router();
-  const SCAFF = { url: 'defscaf.glb', scale: 1 };
+  const SCAFF = { url: 'scof1.glb', scale: 1 };
 
   router.get('/proposals/:id', (req, res) => {
     try {
